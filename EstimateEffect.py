331c2ad0a9e773951c0440a18cef29fab159b28b
Shamanth-GiT <shamanth.kuthpadi2003@gmail.com>
2024-08-21 12:14:23 -0400

    mechanism if estimate is rejected

--- EstimateEffect.py
+++ EstimateEffect.py
@@ -4,6 +4,8 @@ and find the best causal model for the given dataset
 '''
 
 from causallearn.search.ConstraintBased.PC import pc
+from causallearn.search.ScoreBased.GES import ges
+from causallearn.search.FCMBased import lingam
 from causallearn.utils.PDAG2DAG import pdag2dag
 from util import *
 import dowhy.gcm.falsify
@@ -23,15 +25,38 @@ class EstimateEffect:
         self.estimate = 0
         self.pval = None
         self.graph_ref = None
+        self.algos = ['pc', 'ges', 'icalingam']
+        self.idx = 0
 
     def _create_cgm(self):
+        algorithm = self.algos[self.idx]
         try:
-            df = self.data.to_numpy()
-            labels = self.data.columns
-            cg = pc(data=df, show_progress=False, node_names=labels)
-            cg = pdag2dag(cg.G)
-            predicted_graph = genG_to_nx(cg, labels)
-            self.graph = predicted_graph
+            match algorithm:
+                case 'pc':
+                    df = self.data.to_numpy()
+                    labels = self.data.columns
+                    cg = pc(data=df, show_progress=False, node_names=labels)
+                    cg = pdag2dag(cg.G)
+                    predicted_graph = genG_to_nx(cg, labels)
+                    self.graph = predicted_graph
+                    return
+                case 'ges':
+                    df = self.data.to_numpy()
+                    labels = self.data.columns
+                    cg = ges(data=df, show_progress=False, node_names=labels)
+                    cg = pdag2dag(cg['G'])
+                    predicted_graph = genG_to_nx(cg, labels)
+                    self.graph = predicted_graph
+                    return
+                case 'icalingam':
+                    model = lingam.ICALiNGAM()
+                    model.fit(df)
+                    pyd_lingam = pyd_lingam.pipe(format='dot').decode('utf-8')
+                    pyd_lingam = (pyd_lingam,) = graph_from_dot_data(pyd_lingam)
+                    dot_data_lingam = pyd_lingam.to_string()
+                    predicted_graph = genG_to_nx(dot_data_lingam, labels)
+                    self.graph = predicted_graph
+
         except Exception as e:
             print(f"Error in creating causal graph: {e}")
             raise
@@ -76,13 +101,13 @@ class EstimateEffect:
             print(f"Error in estimating effect: {e}")
             raise
 
-    def _refute_estimate(self, method_name="placebo_treatment_refuter", placebo_type="permute"):
+    def _refute_estimate(self, method_name="placebo_treatment_refuter"):
         try:
             refute_placebo_treatment = self.model.refute_estimate(
                 self.estimand,
                 self.estimate,
                 method_name=method_name,
-                placebo_type=placebo_type
+                placebo_type="permute"
             )
             self.pval = refute_placebo_treatment
         except Exception as e:
@@ -96,28 +121,37 @@ class EstimateEffect:
         print(self.pval)
 
     def perform_effect_estimation(self, estimation_method="backdoor.linear_regression", refutation_method="placebo_treatment_refuter"):
-        try:
-            # Create the causal graph
-            self._create_cgm()
-            
-            # Refute the causal graph and apply suggestions
-            self._refute_graph()
+        significant = True
+        while significant is True and self.idx < len(self.algos):
+            try:
+                # Create the causal graph
+                self._create_cgm()
+                
+                # Refute the causal graph and apply suggestions
+                self._refute_graph()
 
-            # Identify the estimand
-            self._identify_effect()
+                # Identify the estimand
+                self._identify_effect()
 
-            # Estimate the effect
-            self._estimate(method_name=estimation_method)
+                # Estimate the effect
+                self._estimate(method_name=estimation_method)
 
-            # Refute the estimate
-            self._refute_estimate(method_name=refutation_method)
+                # Refute the estimate
+                self._refute_estimate(method_name=refutation_method)
 
-            print(self.estimate)
+                print(self.estimate)
 
-            return self.estimate
-        except Exception as e:
-            print(f"Error during effect estimation: {e}")
-            raise
+                if self.pval.refutation_result['is_statistically_significant'] is False:
+                    significant = False
+                    return self.estimate
+                else:
+                    if self.idx == len(self.algos)-1:
+                        print("Best estimate found, but it should be considered with caution.")
+                        return self.estimate
+                    self.idx += 1
+            except Exception as e:
+                print(f"Error during effect estimation with {self.algos[self.idx]}: {e}")
+                raise
 
 
 
